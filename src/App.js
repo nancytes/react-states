b4322ce666496b93121668ad63067a3c0fd1df8f
Nancy Tesfaye <nancy23tes@gmail.com>
2024-06-29 18:51:16 +0300

    change on the app.js

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ function App() {
   return (
     <div className="App">
       {/* <UsingClassLifeCycleComponent /> */}
-      {/* <Mycounter /> */}
-      {/* <IncreaseDecreaseCount /> */}
+      <Mycounter />
+      <IncreaseDecreaseCount />
       <UseEffectForTitle />
       
     </div>
